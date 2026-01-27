69d358e8efdd9a13ed47d90366776a2dc2ff07fd
Denis Rykov <drycov@gmail.com>
2026-01-27 19:39:13 +0500

    feat: Обновить экспортируемые модели и сервисы в init.py

--- src/camera_probe/__init__.py
+++ src/camera_probe/__init__.py
@@ -1,4 +1,15 @@
 from camera_probe.bootstrap.probe import build_probe_service
 from camera_probe.bootstrap.scan import build_scan_service
+from camera_probe.domain.models.detect_result import DetectResult
+from camera_probe.domain.models.probe_result import ProbeResult
+from camera_probe.domain.models.network_info import NetworkInfo
+from camera_probe.domain.models.ntp_info import NtpInfo
 
-__all__ = ["build_probe_service","build_scan_service"]
+__ALL__ = [
+    "DetectResult",
+    "ProbeResult",
+    "NetworkInfo",
+    "NtpInfo",
+    "build_probe_service",
+    "build_scan_service",
+]
